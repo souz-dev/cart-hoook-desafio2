458efefc4debf7a275533c20148352054afeed71
souz-dev <souz.developer@gmail.com>
2022-01-26 00:23:55 -0300

    finish test

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { MdShoppingBasket } from "react-icons/md";
+import { Link } from "react-router-dom";
+import logo from "../../assets/images/logo.svg";
+import { useCart } from "../../hooks/useCart";
+import { Cart, Container } from "./styles";
+
+const Header = (): JSX.Element => {
+  const { cart } = useCart();
+  const cartSize = cart.length;
+
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span data-testid="cart-size">
+            {cartSize === 1 ? `${cartSize} item` : `${cartSize} itens`}
+          </span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+};
+
+export default Header;
